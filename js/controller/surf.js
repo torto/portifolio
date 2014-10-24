fe47ccaf02a595e03b3e9dc782fec711485d0416
torto <tortoyoyo@gmail.com>
2014-10-24 15:26:23 -0200

    adicionado movimentos básicos

--- js/controller/surf.js
+++ js/controller/surf.js
@@ -12,11 +12,11 @@ var app = {
 
         this.carregarOndas();
         this.carregarSurfista();
-        
+
     },
     carregarOndas: function() {
         // MAR CONFIGURAÇAO
-        var ttMar = PIXI.Texture.fromImage("img/mar/1.png");
+        var ttMar = PIXI.Texture.fromImage("img/mar/1-test.png");
         this.elMar = new PIXI.TilingSprite(ttMar, 1000, 71);
 
         this.elMar.position.y = 429;
@@ -39,7 +39,7 @@ var app = {
         this.gameInjecaoMar.addChild(this.elMar3);
         this.gameInjecaoMar.addChild(this.elMar2);
         this.gameInjecaoMar.addChild(this.elMar1);
-        this.gameInjecaoMar.addChild(this.elMar); 
+        this.gameInjecaoMar.addChild(this.elMar);
 
     },
     carregarSurfista: function() {
@@ -48,15 +48,15 @@ var app = {
         var ttSurfista = PIXI.Texture.fromImage("img/surfista.png");
         this.elSurfista = new PIXI.Sprite(ttSurfista);
 
-        this.elSurfista.position.x = 100;
-        this.elSurfista.position.y = 100;
+        this.elSurfista.position.x = 30;
+        this.elSurfista.position.y = 260;
 
         this.gameInjecao.addChild(this.elSurfista);
     },
     inserirStage: function() {
         //inserir imagens e elementos no stage
-        this.stage.addChild(this.gameInjecao);
         this.stage.addChild(this.gameInjecaoMar);
+        this.stage.addChild(this.gameInjecao);
     },
     iniciarAnimacao: function() {
         //INICIAR ANIMACAO JOGO
@@ -64,7 +64,6 @@ var app = {
     },
     animate: function() {
         //MAR ANIMACAO CONSTANTE
-        // console.log(this);
         app.elMar.tilePosition.x += 1.5;
         app.elMar1.tilePosition.x += 1;
         app.elMar2.tilePosition.x += 1;
@@ -72,9 +71,36 @@ var app = {
         //------------------------
         app.renderer.render(app.stage);
         requestAnimFrame(app.animate);
+    },
+    carregarInicioJogo: function() {
+        this.play = true;
+        controlesJogo.controleSurfista();
+    }
+}
+
+var controlesJogo = {
+    controleSurfista: function() {
+        Mousetrap.bind('right', function() {
+            app.elSurfista.position.x += 5;
+        });
+        Mousetrap.bind('left', function() {
+            app.elSurfista.position.x -= 5;
+        });
+       
+        Mousetrap.bind('up', function() {
+            for (var i = 0; i <= 10; i++) {
+                   app.elSurfista.position.y -= 5;
+            };
+        });
+        Mousetrap.bind('down', function() {
+            for (var i = 0; i <= 10; i++) {
+                   app.elSurfista.position.y += 5;
+            };
+        });
     }
 }
 
 app.inicializar();
 app.inserirStage();
 app.iniciarAnimacao();
+app.carregarInicioJogo();
